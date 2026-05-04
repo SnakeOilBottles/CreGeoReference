a899029c047b60ed5bcf57073d087bd2e844c543
KMicha <michael.kahle@yahoo.de>
2026-05-04 16:14:04 +0200

    check for errors

--- test_production.sh
+++ test_production.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 pip3 install CreGeoReference
-python -m unittest
+
+
+#python -m unittest
+resultsFound=$(python3 -m unittest 2>&1)
+
 pip3 uninstall -y CreGeoReference
 pip3 install CreGeoReference
+
+hasError=$(echo $resultsFound | grep -c 'ERROR\|FAIL')
+if [ $hasError -eq 0 ]; then
+    echo $resultsFound
+    echo "[SUCCESS]: unittests succeeded!"
+    exit 0
+else
+    echo $resultsFound
+    echo "[FAIL]: Errors in unittests!"
+    #echo $resultsFound | grep 'ERROR\|FAIL'
+    exit 1
+fi
--- test_test.sh
+++ test_test.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple CreGeoReference
-python -m unittest
+python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple CreGeoReference
+
+#python -m unittest
+resultsFound=$(python3 -m unittest 2>&1)
+
 pip3 uninstall -y CreGeoReference
 pip3 install CreGeoReference
+
+hasError=$(echo $resultsFound | grep -c 'ERROR\|FAIL')
+if [ $hasError -eq 0 ]; then
+    echo $resultsFound
+    echo "[SUCCESS]: unittests succeeded!"
+    exit 0
+else
+    echo $resultsFound
+    echo "[FAIL]: Errors in unittests!"
+    #echo $resultsFound | grep 'ERROR\|FAIL'
+    exit 1
+fi
